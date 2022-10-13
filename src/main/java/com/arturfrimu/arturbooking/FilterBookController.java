a23feffaeaaffaa361f2c38eee67f819b1dc6a1a
Artur Frimu <artur.frimu2@gmail.com>
2022-10-13 16:35:07 +0300

    feat: get file extensions

--- src/main/java/com/arturfrimu/arturbooking/FilterBookController.java
+++ src/main/java/com/arturfrimu/arturbooking/FilterBookController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -19,6 +20,16 @@ public class FilterBookController {
     public Set<String> filterByExtension(@RequestBody Set<String> extension) {
         File folder = new File("D:\\Artur\\books\\Business\\files");
         File[] listOfFiles = folder.listFiles();
-        return Arrays.stream(listOfFiles).map(File::getName).collect(Collectors.toSet());
+
+        return Arrays.stream(listOfFiles)
+                .map(File::getName)
+                .map(fileName -> getExtensionByStringHandling(fileName).orElse(""))
+                .collect(Collectors.toSet());
+    }
+
+    public Optional<String> getExtensionByStringHandling(String filename) {
+        return Optional.ofNullable(filename)
+                .filter(f -> f.contains("."))
+                .map(f -> f.substring(filename.lastIndexOf(".") + 1));
     }
 }
